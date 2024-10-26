a8ae145c39a89f3e62ab5e5691f24da12d3574eb
basantsaed2 <basantsaed718@gmail.com>
2024-10-26 11:14:37 +0300

    fcommit

--- src/Pages/Request/RequestPage.jsx
+++ src/Pages/Request/RequestPage.jsx
@@ -379,52 +379,99 @@ const RequestPage = () => {
                                         </Link>
                                         </td>
                                         <td className="min-w-[100px] sm:min-w-[80px] sm:w-1/12 lg:w-1/12 py-2 text-center text-thirdColor text-sm sm:text-base lg:text-lg xl:text-xl overflow-hidden">
-                                        <div className="flex items-center justify-center gap-x-3">
-                                        <Link to={`edit/${request.id}`} state={request.id} type="button">
-                                        <EditIcon />
-                                        </Link>
-                                        <button type="button" onClick={() => handleOpenDialog(request.id)}>
-                                        <DeleteIcon />
-                                        </button>
-                                        {openDialog === request.id && (
-                                        <Dialog open={true} onClose={handleCloseDialog} className="relative z-10">
-                                                <DialogBackdrop className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-                                                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-                                                <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                                                <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg">
-                                                <div className="flex flex-col items-center justify-center bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                                                        <Wroning Width='28' Height='28' aria-hidden="true" />
-                                                        <div className="flex items-center">
-                                                        <div className="mt-2 text-center">
-                                                        <DialogTitle as="h3" className="text-xl font-semibold leading-10 text-gray-900">
-                                                        You will delete request {request.user_name || "null"}
-                                                        </DialogTitle>
+                                                {/* <div className="flex items-center justify-center gap-x-3">
+                                                <Link to={`edit/${request.id}`} state={request.id} type="button">
+                                                <EditIcon />
+                                                </Link>
+                                                <button type="button" onClick={() => handleOpenDialog(request.id)}>
+                                                <DeleteIcon />
+                                                </button>
+                                                {openDialog === request.id && (
+                                                <Dialog open={true} onClose={handleCloseDialog} className="relative z-10">
+                                                        <DialogBackdrop className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                                                        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                                                        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                                        <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg">
+                                                        <div className="flex flex-col items-center justify-center bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                                                <Wroning Width='28' Height='28' aria-hidden="true" />
+                                                                <div className="flex items-center">
+                                                                <div className="mt-2 text-center">
+                                                                <DialogTitle as="h3" className="text-xl font-semibold leading-10 text-gray-900">
+                                                                You will delete request {request.user_name || "null"}
+                                                                </DialogTitle>
+                                                                </div>
+                                                                </div>
+                                                        </div>
+                                                        <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                                                <button
+                                                                type="button"
+                                                                onClick={() => handleDelete(request.id)}
+                                                                disabled={isDeleting}
+                                                                className="inline-flex w-full justify-center rounded-md bg-mainColor px-6 py-3 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto"
+                                                                >
+                                                                {isDeleting ? <div className="flex w-10 h-5 justify-center items-center"><LoadingSpinner /></div> : 'Delete'}
+                                                                </button>
+                                                                <button
+                                                                type="button"
+                                                                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-6 py-3 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
+                                                                onClick={handleCloseDialog}
+                                                                >
+                                                                Cancel
+                                                                </button>
+                                                        </div>
+                                                        </DialogPanel>
                                                         </div>
                                                         </div>
+                                                </Dialog>
+                                                )}
+                                                </div> */}
+                                                <div className="flex items-center justify-center gap-x-3">
+                                                <Link to={`edit/${request.id}`} state={request.id} type="button">
+                                                        <EditIcon />
+                                                </Link>
+                                                <button type="button" onClick={() => handleOpenDialog(request.id)}>
+                                                        <DeleteIcon />
+                                                </button>
+                                                {openDialog === request.id && (
+                                                        <Dialog open={true} onClose={handleCloseDialog} className="relative z-10">
+                                                                <DialogBackdrop className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+                                                                <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+                                                                        <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                                                                                <DialogPanel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:max-w-lg">
+                                                                                <div className="flex flex-col items-center justify-center bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                                                                                        <Wroning Width='28' Height='28' aria-hidden="true" />
+                                                                                        <div className="flex items-center">
+                                                                                                <div className="mt-2 text-center">
+                                                                                                        <DialogTitle as="h3" className="text-xl font-semibold leading-10 text-gray-900">
+                                                                                                                You will delete request {request.user_name|| "null"}
+                                                                                                        </DialogTitle>
+                                                                                                </div>
+                                                                                        </div>
+                                                                                </div>
+                                                                                <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                                                                                        <button
+                                                                                                type="button"
+                                                                                                onClick={() => handleDelete(request.id)}
+                                                                                                disabled={isDeleting}
+                                                                                                className="inline-flex w-full justify-center rounded-md bg-mainColor px-6 py-3 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto"
+                                                                                        >
+                                                                                                {isDeleting ? <div className="flex w-10 h-5"><Loading /></div> : 'Delete'}
+                                                                                        </button>
+                                                                                        <button
+                                                                                                type="button"
+                                                                                                data-autofocus
+                                                                                                onClick={handleCloseDialog}
+                                                                                                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-6 py-3 text-sm font-medium text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
+                                                                                        >
+                                                                                                Cancel
+                                                                                        </button>
+                                                                                </div>
+                                                                                </DialogPanel>
+                                                                        </div>
+                                                                </div>
+                                                        </Dialog>
+                                                )}
                                                 </div>
-                                                <div className="px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                                                        <button
-                                                        type="button"
-                                                        onClick={() => handleDelete(request.id)}
-                                                        disabled={isDeleting}
-                                                        className="inline-flex w-full justify-center rounded-md bg-mainColor px-6 py-3 text-sm font-semibold text-white shadow-sm sm:ml-3 sm:w-auto"
-                                                        >
-                                                        {isDeleting ? <div className="flex w-10 h-5 justify-center items-center"><LoadingSpinner /></div> : 'Delete'}
-                                                        </button>
-                                                        <button
-                                                        type="button"
-                                                        className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-6 py-3 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 sm:mt-0 sm:w-auto"
-                                                        onClick={handleCloseDialog}
-                                                        >
-                                                        Cancel
-                                                        </button>
-                                                </div>
-                                                </DialogPanel>
-                                                </div>
-                                                </div>
-                                        </Dialog>
-                                        )}
-                                        </div>
                                         </td>
                                 </tr>
                                 ))}
@@ -518,7 +565,7 @@ const RequestPage = () => {
                                                                                         <div className="flex items-center">
                                                                                                 <div className="mt-2 text-center">
                                                                                                         <DialogTitle as="h3" className="text-xl font-semibold leading-10 text-gray-900">
-                                                                                                                You will delete request {request.user?.name|| "null"}
+                                                                                                                You will delete request {request.user_name|| "null"}
                                                                                                         </DialogTitle>
                                                                                                 </div>
                                                                                         </div>
@@ -633,7 +680,7 @@ const RequestPage = () => {
                                                                                         <div className="flex items-center">
                                                                                                 <div className="mt-2 text-center">
                                                                                                         <DialogTitle as="h3" className="text-xl font-semibold leading-10 text-gray-900">
-                                                                                                                You will delete request {request.user?.name|| "null"}
+                                                                                                                You will delete request {request.user_name|| "null"}
                                                                                                         </DialogTitle>
                                                                                                 </div>
                                                                                         </div>
